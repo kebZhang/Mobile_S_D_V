e29dfc2c8b8a28c1d6009563889376445d88a151
TyZhangSDU <3260603105@qq.com>
2024-11-18 00:14:18 -0500

    1118-lenovo update try1

---
+++ asn1_group/B163_cb_dcall.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "change_byte_sequence.h"
+#include "LTE-ML1-PDSCH-Stat-Indication-S-H.h"
+#include "LTE-ML1-PDSCH-Stat-Indication-R-H.h"
+#include "LTE-ML1-PDSCH-Stat-Indication-TB.h"
+#include "LTE-ML1-PDSCH-Stat-Indication-R-T.h"
+#include "B163_cb_dcall.h"
